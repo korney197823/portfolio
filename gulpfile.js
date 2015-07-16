e2b866ff1689d5b0504994ef1f5298f5b0872a96
Kornienko Denis <korney197823@gmail.com>
2015-07-16 14:44:25 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp"),
+	browserSync = require('browser-sync');
+
+gulp.task('server', function () {
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'src'
+		}
+	});
+});
+
+gulp.task('watch', function () {
+	gulp.watch([
+		'src/*.index.html',
+		'src/js/**/*.js',
+		'src/css/**/*.css'
+		]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
