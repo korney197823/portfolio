87b67602190839832e76e503fe80f44ffa4bb922
Kornienko Denis <korney197823@gmail.com>
2015-08-02 18:01:39 +0300

    finish contact-me page

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+var	myModule = (function () {
+
+	var init = function() {
+		_setUpListners();
+	    };
+
+	var _setUpListners = function () {
+		$('#add-new-item').on('click', _showModal); //открыть модальное окно
+		};
+
+	var _showModal = function () {
+
+	};
+
+	return {
+		init: init;
+		}
+	};
+
+})();
+
+myModule.init();
